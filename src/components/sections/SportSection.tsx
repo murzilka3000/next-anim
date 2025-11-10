deee6f5368005ca2e20973638cb2bf429572907e
Nikolay <nikolayzhidkov@icloud.com>
2025-11-10 16:14:08 +0700

    100

--- src/components/sections/SportSection.tsx
+++ src/components/sections/SportSection.tsx
@@ -99,7 +99,7 @@ export const SportSection: React.FC = () => {
             ease: "none",
             scrollTrigger: {
               trigger: section,
-              start: "top 15%",
+              start: "top -8%",
               end: () => `+=${delta + 200}`,
               pin: true,
               pinSpacing: true,
